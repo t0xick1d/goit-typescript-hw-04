4f86ac60a290450fc5097375e52a827d05f59ff7
zheniaStukalo <82517499+t0xick1d@users.noreply.github.com>
2023-10-31 21:15:50 +0200

    complite 2 task

--- src/homework/1.tsx
+++ src/homework/1.tsx
@@ -1,40 +1,50 @@
 import React, { useEffect, useRef } from 'react';
 
 // Опишіть Props
+
+type Props = {
+   children: React.ReactElement;
+   onContentEndVisible: () => void;
+};
+class OptionT {
+   constructor(public rootMargin: string, public threshold: number, public root: null) {}
+}
+
 export function Observer({ children, onContentEndVisible }: Props) {
-  // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
-  const endContentRef = useRef(null);
+   // Вкажіть правильний тип для useRef зверніть увагу, в який DOM елемент ми його передаємо
 
-  useEffect(() => {
-    // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0,
-      root: null,
-    };
+   const endContentRef = useRef<HTMLInputElement>(null);
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (entry.intersectionRatio > 0) {
-          onContentEndVisible();
-          observer.disconnect();
-        }
-      });
-    }, options);
+   useEffect(() => {
+      // Вкажіть правильний тип для options, підказка, клас також можна вказувати як тип
+      const options: OptionT = {
+         rootMargin: '0px',
+         threshold: 1.0,
+         root: null,
+      };
 
-    if (endContentRef.current) {
-      observer.observe(endContentRef.current);
-    }
+      const observer = new IntersectionObserver((entries) => {
+         entries.forEach((entry) => {
+            if (entry.intersectionRatio > 0) {
+               onContentEndVisible();
+               observer.disconnect();
+            }
+         });
+      }, options);
 
-    return () => {
-      observer.disconnect();
-    };
-  }, [onContentEndVisible]);
+      if (endContentRef.current) {
+         observer.observe(endContentRef.current);
+      }
 
-  return (
-    <div>
-      {children}
-      <div ref={endContentRef} />
-    </div>
-  );
+      return () => {
+         observer.disconnect();
+      };
+   }, [onContentEndVisible]);
+
+   return (
+      <div>
+         {children}
+         <div ref={endContentRef} />
+      </div>
+   );
 }
